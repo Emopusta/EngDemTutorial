70aba9d754eb15c484ec3ced5dc28cf7c4e4cc40
Emopusta <emreduman225@hotmail.com>
2022-06-22 16:23:24 +0300

    initial commit

---
+++ EngDemTutorialProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EngDemTutorialProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object temp = "emre";
+            Console.WriteLine(temp);
+        }
+    }
+}
